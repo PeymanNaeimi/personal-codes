edad90899fb99c8f9d3cc2ccfc1aaba0ca1aef83
Aleph (AKA no_itsmyturn) <67456590+therealaleph@users.noreply.github.com>
2022-10-06 07:44:25 +0000

    Create iran.sh

---
+++ iran.sh
@@ -0,0 +1,10 @@
+COUNTRIES=('ir')
+ipset flush countries
+for i in "${COUNTRIES[@]}"; do
+    echo "Ban IP of country ${i}"
+
+    for IP in $(wget --no-check-certificate -O - https://www.ipdeny.com/ipblocks/data/countries/${i}.zone)
+    do
+        ipset add countries $IP,22;
+    done
+done
